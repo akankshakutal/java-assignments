e0e4f3437918ddc2ed78d82739834941994b908d
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-03-14 10:24:17 +0530

    Implement class setDemo

--- src/com/step/assignment/SetDemo/Sets.java
+++ src/com/step/assignment/SetDemo/Sets.java
@@ -1,4 +1,27 @@
 package com.step.assignment.SetDemo;
 
-public class Sets {
+import java.util.HashSet;
+import java.util.Set;
+
+public class Sets<T> {
+
+
+    public Set<T> getUnion(Set<T> set1, Set<T> set2){
+        Set<T> union = new HashSet<>(set1);
+        union.addAll(set1);
+        union.addAll(set2);
+        return union;
+    }
+
+    public Set<T> getIntersection(Set<T> set1,Set<T> set2){
+        Set<T> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        return intersection;
+    }
+
+    public Set<T> getDifference(Set<T> set1,Set<T> set2){
+        Set<T> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+        return difference;
+    }
 }
